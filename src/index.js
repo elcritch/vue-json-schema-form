44a1ab4dec51397d1305944e61aee160347241a3
Jaremy Creechley <creechley@gmail.com>
2017-05-20 22:33:32 -0600

    getting setup.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+// import 'todomvc-app-css/index.css';
+import VueBlu from 'vue-blu'
+import 'vue-blu/dist/css/vue-blu.min.css'
+
+Vue.use(VueBlu)
+
+export default new Vue({
+  el: '#root'
+});
